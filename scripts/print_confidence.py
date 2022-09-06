011c05f14c73ee96a09f56469f804319aa2c5d1c
Luke Ross <lukeross@umich.edu>
2022-09-05 20:22:12 -0400

    Temporarily remove fte from script until 2022 predictions are added

--- scripts/print_confidence.py
+++ scripts/print_confidence.py
@@ -1,6 +1,7 @@
 import argparse
 
 import pandas as pd
+import numpy as np
 
 from nfl_confidence.html_parsers.espn import get_week_espn_confidence
 from nfl_confidence.html_parsers.fte import get_week_538_confidence
@@ -36,29 +37,33 @@ args = parser.parse_args()
 
 # Get confidence rankings
 espn_confidence = get_week_espn_confidence(args.week, skip_errors=args.skip_errors)
-fte_confidence = get_week_538_confidence(args.week, skip_errors=args.skip_errors)
+# fte_confidence = get_week_538_confidence(args.week, skip_errors=args.skip_errors)
 
 # Re-map names
 nickname_to_official = load_team_name_map()
-for conf_list in [espn_confidence, fte_confidence]:
+for conf_list in [espn_confidence]:#, fte_confidence]:
     for i in range(len(conf_list)):
         for team_name in conf_list[i].keys():
             conf_list[i][nickname_to_official[team_name]] = conf_list[i].pop(team_name)
 
 # Get any extra games missed by either source
 espn_games = set([frozenset(game.keys()) for game in espn_confidence])
-fte_games = set([frozenset(game.keys()) for game in fte_confidence])
-espn_missing = fte_games - espn_games
-fte_missing = espn_games - fte_games
-for missing_games, conf_list in zip([espn_missing, fte_missing], [espn_confidence, fte_confidence]):
-    for missing_game in missing_games:
-        conf_list.append({team: None for team in missing_game})
+# fte_games = set([frozenset(game.keys()) for game in fte_confidence])
+# espn_missing = fte_games - espn_games
+# fte_missing = espn_games - fte_games
+# for missing_games, conf_list in zip([espn_missing, fte_missing], [espn_confidence, fte_confidence]):
+#     for missing_game in missing_games:
+#         conf_list.append({team: None for team in missing_game})
 
 # Sort games for every confidence source
 espn_confidence = sorted(espn_confidence, key=lambda x: sorted(list(x.keys()))[0])
-fte_confidence = sorted(fte_confidence, key=lambda x: sorted(list(x.keys()))[0])
+# fte_confidence = sorted(fte_confidence, key=lambda x: sorted(list(x.keys()))[0])
+fte_confidence = espn_confidence
 
 # Compute winners and averaged probabilities
+espn_probs = []
+fte_probs = []
+variance = []
 confidence_probs = []
 predicted_winners = []
 for i in range(len(espn_confidence)):
@@ -77,6 +82,9 @@ for i in range(len(espn_confidence)):
         assert team_a_prob + team_b_prob == 100
         winner = team_a if team_a_prob >= team_b_prob else team_b
         confidence_probs.append(max(team_a_prob, team_b_prob))
+        espn_probs.append(espn_confidence[i][winner])
+        fte_probs.append(fte_confidence[i][winner])
+        variance.append(np.var([espn_probs[-1], fte_probs[-1]]))
         predicted_winners.append(winner)
 
 # Compute confidence ranks
@@ -90,6 +98,9 @@ df = pd.DataFrame(
         "team_a": [list(game.keys())[0] for game in espn_confidence],
         "team_b": [list(game.keys())[1] for game in espn_confidence],
         "predicted_winner": predicted_winners,
+        "espn_prob": espn_probs,
+        "fte_prob": fte_probs,
+        "variance": variance,
         "confidence_prob": confidence_probs,
         "confidence_rank": confidence_ranks,
     }
